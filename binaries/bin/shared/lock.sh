9ab51ee9c8801810756ba4ad5ba389ba51561276
orestisf <orestisf1993@gmail.com>
2017-04-09 21:58:14 +0300

    Update lock scripts
    - i3lock -> i3lock-cac03-git
    - Move logo upwards.
    - Common file in ~/bin/shared.

---
+++ binaries/bin/shared/lock.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+SCREEN=/tmp/screen.png
+LOGO=$HOME/.local/share/logo.png
+RES=${LOCK_RESOLUTION}
+FILTER="boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/4"
+SCREENSHOT_CMD="ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOGO -filter_complex \"$FILTER\" -vframes 1 $SCREEN -loglevel quiet"
+I3LOCK_CMD="i3lock -i $SCREEN -t -n"
