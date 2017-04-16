5fb75005a4eb813f3f02b1570ae048f512f2a7bc
orestisf <orestisf1993@gmail.com>
2017-04-16 04:18:03 +0300

    Modify backup.sh to run backup-ocean.sh as well

--- binaries/bin/backup.sh
+++ binaries/bin/backup.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -e
 
 # name of the repo and archive name
 BORG_REPO='/media/shared/Backup/borg'
-PREFIX="home-of-`hostname`"
-DATE="`date +%Y-%m-%d_%H-%M`"
+PREFIX="home-of-$(hostname)"
+DATE=$(date +%Y-%m-%d_%H-%M)
 NAME="$PREFIX-$DATE"
 
 echo "Executing borg create:"
 # compression lzma is the slowest.
 borg create --verbose --stats --show-rc   \
-    --exclude *.pyc                       \
+    --exclude "*.pyc"                     \
     --exclude /home/*/.cache              \
     --exclude /home/*/.gvfs/              \
     --exclude /home/*/.local/share/Trash/ \
@@ -18,19 +19,22 @@ borg create --verbose --stats --show-rc   \
     --exclude /home/*/.local/share/Steam/ \
     --exclude /home/*/.AMD/               \
     --exclude /home/*/.wine/              \
+    --exclude /home/swapfile              \
     --compression lzma                    \
     --chunker-params 19,23,21,4095        \
-    $BORG_REPO::$NAME $HOME
+    $BORG_REPO::"$NAME" "$HOME"
 
 echo "Executing borg prune:"
 # Use the `prune` subcommand to maintain 3 daily, 4 weekly, 6 monthly and 1 yearly
 # archives of THIS machine. --prefix `hostname`- is very important to limit prune's
 # operation to this machine's archives and not apply to other machine's archives also.
 borg prune --verbose --stats --show-rc \
-    --prefix $PREFIX                   \
+    --prefix "$PREFIX"                 \
     --keep-within=2d                   \
     --keep-daily=3                     \
     --keep-weekly=10                   \
     --keep-monthly=20                  \
     --keep-yearly=1                    \
     $BORG_REPO
+
+source "$HOME/bin/backup-ocean.sh"
