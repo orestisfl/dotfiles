af1c9ee7cacec7a9a7d259f49bc810014e8ff0ee
orestisf <orestisf1993@gmail.com>
2017-05-31 02:43:58 +0300

    Add script for workspace renaming

---
+++ binaries/bin/i3-workspace-rename.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import subprocess
+import i3ipc
+
+i3 = i3ipc.Connection()
+workspaces = i3.get_workspaces()
+for workspace in workspaces:
+    if workspace["focused"]:
+        num = workspace['num']
+        print(f"Renaming workspace number {num}")
+        assert(num > 0)
+        new_name = subprocess.check_output("zenity --entry", shell=True).decode()
+        i3.command(f"rename workspace to {num}:{new_name}")
