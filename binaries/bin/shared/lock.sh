3a8e5256c41faba6c4b9873ed37a504a167dbde0
orestisf <orestisf1993@gmail.com>
2017-07-03 23:06:14 +0300

    binaries: logo.png->lock.png

--- binaries/bin/shared/lock.sh
+++ binaries/bin/shared/lock.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SCREEN=/tmp/screen.png
-LOGO=$HOME/.local/share/logo.png
+LOGO=$HOME/.local/share/lock.png
 RES=${LOCK_RESOLUTION}
 FILTER="boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/4"
 SCREENSHOT_CMD="ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOGO -filter_complex \"$FILTER\" -vframes 1 $SCREEN -loglevel quiet"
