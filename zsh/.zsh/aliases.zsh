b07a17e3b1a402d906f4845db2ec412cd1066325
Orestis Floros <orestisf1993@gmail.com>
2017-08-26 20:30:21 +0300

    zsh: add ls commands to history

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -17,12 +17,12 @@ alias fgrep='fgrep --color=auto'
 
 # ls related
 # all command start with space so they are not written in history
-alias ls=' ls --color=auto'
-alias l=' exa -lh --group-directories-first'
-alias lr=' exa -lh -R --group-directories-first'  # Lists human readable sizes, recursively.
-alias lk=' exa -lh -Sr --group-directories-first' # Lists sorted by size, largest last.
-alias lt=' ls -lh -tr'                            # Lists sorted by date, most recent last.
-alias lc=' lt -c'                                 # Lists sorted by date, most recent last, shows change time.
+alias ls='ls --color=auto'
+alias l='exa -lh --group-directories-first'
+alias lr='exa -lh -R --group-directories-first'  # Lists human readable sizes, recursively.
+alias lk='exa -lh -Sr --group-directories-first' # Lists sorted by size, largest last.
+alias lt='ls -lh -tr'                            # Lists sorted by date, most recent last.
+alias lc='lt -c'                                 # Lists sorted by date, most recent last, shows change time.
 
 # c: Copy to clipboard, v:Paste from clipboard
 alias c="xclip -in -selection clipboard"
