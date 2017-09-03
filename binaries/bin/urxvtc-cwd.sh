c70e0403eb25001abbf130f2a636fbc28013f75e
Orestis Floros <orestisf1993@gmail.com>
2017-09-03 19:22:54 +0300

    i3: remove urxvt --release from bindsym

--- binaries/bin/urxvtc-cwd.sh
+++ binaries/bin/urxvtc-cwd.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 if xprop -id $(xdotool getwindowfocus) WM_CLASS | grep -i urxvt
 then
-    xdotool key Control_L+Return
+    xdotool keyup Return
+    xdotool key --clearmodifiers Control_L+Return
 else
     i3-sensible-terminal
 fi
