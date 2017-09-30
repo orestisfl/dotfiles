3e0047ab683a32b15c866b03b6c2bd4691049501
Orestis Floros <orestisf1993@gmail.com>
2017-09-30 05:29:23 +0300

    start-xephyr: add i3 build path to $PATH

--- xorg/start-xephyr.sh
+++ xorg/start-xephyr.sh
@@ -8,6 +8,8 @@ export GTK_OVERLAY_SCROLLING=0
 export STEAM_RUNTIME=0
 D="${D:-100}"
 export RXVT_SOCKET=/tmp/urxvtd-$D.socket
+i3_build="$HOME/Documents/programming/i3/build"
+export PATH="$i3_build/:$i3_build/i3-config-wizard:$i3_build/i3-dump-log:$i3_build/i3-input:$i3_build/i3-msg:$i3_build/i3-nagbar:$i3_build/i3bar:$PATH"
 
 xephyr_name(){
     echo "Xephyr_$D"
@@ -61,12 +63,12 @@ trap finish INT
 echo "Passing args '$*' to i3"
 if [[ -n "$ISSUE_I3" ]]; then
     i3 --moreversion 2>&- || i3 --version
-    "$HOME/Documents/programming/i3/build/i3" -c ~/Desktop/default.config --shmlog-size=26214400 "$@"
+    i3 -c ~/Desktop/default.config --shmlog-size=26214400 "$@"
     i3-dump-log | vipe | bzip2 -c | curl --data-binary @- http://logs.i3wm.org
 elif [[ -n "$GDB_I3" ]]; then
-    gdb --args "$HOME/Documents/programming/i3/build/i3" -c /tmp/i3.config -V -d all "$@"
+    gdb --args i3 -c /tmp/i3.config -V -d all "$@"
 else
-    "$HOME/Documents/programming/i3/build/i3" -c /tmp/i3.config -V "$@" 2>&1 &
+    i3 -c /tmp/i3.config -V "$@" 2>&1 &
     wait $!
 fi
 finish
