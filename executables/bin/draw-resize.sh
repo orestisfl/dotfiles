adb8abf04e170964368ebf391c2301172eb7b6bf
Orestis Floros <orestisf1993@gmail.com>
2018-03-12 14:35:08 +0200

    draw-resize.sh: remove 'read' call
    From
    https://github.com/Airblader/dotfiles-manjaro/blob/b61b061c96f581db185f9943af7ac1905674f265/scripts/slopsize.sh

--- executables/bin/draw-resize.sh
+++ executables/bin/draw-resize.sh
@@ -6,6 +6,5 @@ set -e -x
 
 window_id="${1:-$(xwininfo | grep 'id: 0x' | grep -Eo '0x[a-z0-9]+')}"
 
-read -r X Y W H < <(slop -f "%x %y %w %h" && echo)
-
+eval $(slop -b 3 -c 0.96,0.5,0.09 -f "X=%x Y=%y W=%w H=%h")
 i3-msg "[id=$window_id]" floating enable, resize set "$W" "$H", move to position "$X" px "$Y" px
