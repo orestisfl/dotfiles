cac7268a9fb59ab68d6626d10a1c2a2aceb4582f
Orestis Floros <orestisf1993@gmail.com>
2018-03-21 14:21:07 +0200

    draw-resize.sh: exit on error (escape)
    eval $() doesn't return exit code

--- executables/bin/draw-resize.sh
+++ executables/bin/draw-resize.sh
@@ -7,4 +7,5 @@ set -e -x
 window_id="${1:-$(xwininfo | grep 'id: 0x' | grep -Eo '0x[a-z0-9]+')}"
 
 eval $(slop -b 3 -c 0.96,0.5,0.09 -f "X=%x Y=%y W=%w H=%h")
+[[ -z "$X" ]] && exit 1
 i3-msg "[id=$window_id]" floating enable, resize set "$W" "$H", move to position "$X" px "$Y" px
