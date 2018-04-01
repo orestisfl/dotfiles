9eda099a3213bef9775a87f40cca402817e49195
Orestis Floros <orestisf1993@gmail.com>
2018-04-01 13:39:28 +0300

    urxvtc-cwd: add automark function

--- executables/bin/urxvtc-cwd.sh
+++ executables/bin/urxvtc-cwd.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+
+function next_mark() {
+    i3-msg -t get_marks | python3 -c'
+import string
+import json
+
+marks = json.loads(input())
+for c in string.ascii_lowercase:
+    if c not in marks:
+        print(c)
+        break
+' | xargs i3-msg mark
+}
+
 if xprop -id $(xdotool getwindowfocus) WM_CLASS | grep -i urxvt
 then
     xdotool keyup Return
@@ -6,3 +20,6 @@ then
 else
     exec i3-sensible-terminal
 fi
+
+automark="${1-0}"
+[[ "$automark" -eq "1" ]] && next_mark
