fd1c759695fad0743471ded64f026edf1b767043
Orestis Floros <orestisf1993@gmail.com>
2019-06-19 16:38:34 +0300

    Remove conky

--- conky/.config/conky##Linux.desktop/conky_lua.lua
+++
@@ -1,7 +0,0 @@
-function conky_format(format, number)
-    return string.format(format, conky_parse(number))
-end
-
-function conky_sformat(format, arg)
-    return string.format(format, conky_parse(arg))
-end
--- conky/.config/conky##Linux.desktop/gpuTemp.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# name1=$(lspci -nnk | grep -i vga)
-name1="HD 7850"
-temp1=$(sensors radeon-pci-0100 | grep "temp1" | grep "+[0-9]*.[0-9]" -o | head -n 1)
-fan1=$(expr $(cat /sys/class/drm/card0/device/hwmon/hwmon2/pwm1)00 / 255)
-echo "$name1\${alignr 30}$temp1°C\${offset 30}$fan1%" | column -t
