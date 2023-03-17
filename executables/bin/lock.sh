fc749ff55caad577e7cf7215ac640f7ad0661b90
Orestis Floros <orestisflo@gmail.com>
2023-03-17 09:24:52 +0100

    Cleanup

--- executables/bin/lock.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-old=$(xkb-switch)
-xkb-switch -s us
-i3lock -t -n -i $(find ~/.local/share/lock-images -type f | shuf -n 1)
-xkb-switch -s "$old"
--- executables/bin/rand_wall.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while true
-do
-    feh --bg-fill -z ~/Pictures/wall
-    sleep 30m
-done
