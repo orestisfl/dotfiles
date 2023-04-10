f5d949660710d32feb9f4da13808de69a3a15339
Orestis Floros <orestis.floros@elastic.co>
2023-04-10 09:21:07 +0200

    i3-next-available-workspace: Start from current

--- executables/bin/i3-next-available-workspace.py
+++ executables/bin/i3-next-available-workspace.py
@@ -2,11 +2,11 @@
 import json
 from subprocess import check_output
 
-workspace_nums = [
-    ws["num"] for ws in json.loads(check_output(["i3-msg", "-t", "get_workspaces"]))
-]
+workspaces = json.loads(check_output(["i3-msg", "-t", "get_workspaces"]))
+workspace_nums = [ws["num"] for ws in workspaces]
+f = next(ws["num"] for ws in workspaces if ws["focused"])
 
-for next_num in range(1, 50):
+for next_num in range(f + 1, 50):
     if next_num not in workspace_nums:
         print(next_num)
         break
