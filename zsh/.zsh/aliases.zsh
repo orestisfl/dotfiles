a428d45d23ef044165b0bff85ae762facdafa8c6
Orestis Floros <orestis.floros@elastic.co>
2023-06-23 08:07:02 +0200

    zsh: Update aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,9 +1,9 @@
 # Subtitute old software with modern and improve some commands.
 alias cat=bat
 alias diff=colordiff
-alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
+alias grep='grep --color=auto'
 alias rename=perl-rename
 alias vi=vim
 alias vim=nvim
@@ -23,15 +23,17 @@ alias v="(xclip -o -selection clipboard || xclip -o) | sed -e '\$a\'"
 
 # Shortcuts
 alias autoremove='sudo pacman -Rns $(pacman -Qdtq)'
-alias mksrcinfo='makepkg --printsrcinfo > .SRCINFO'
 alias drm='docker run -it --rm'
 alias drmv='docker run -it --rm -v "$PWD:/workdir" --workdir /workdir'
+alias ghis='gh issue list -L 100 | fzf --preview "gh issue view {+1}"'
+alias ghpr="gh pr list | fzf --preview 'gh pr diff --color=always {+1}' | awk '{print \$1}' | xargs --no-run-if-empty gh pr checkout"
+alias mksrcinfo='makepkg --printsrcinfo > .SRCINFO'
 alias pipupg="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
 alias sdi='systemd-inhibit --what=handle-lid-switch'
-alias ydl=yt-dlp
-alias ydl-music-mp3="yt-dlp -f bestaudio -x --audio-format mp3"
-alias ydl-music="yt-dlp -f bestaudio -x"
+alias ydl-music-mp3='yt-dlp -f bestaudio -x --audio-format mp3'
+alias ydl-music='yt-dlp -f bestaudio -x'
 alias ydl-playlist="yt-dlp -f best --write-srt -o '%(autonumber)s-%(title)s.%(ext)s'"
+alias ydl=yt-dlp
 
 # Typos
 alias υαυ=yay
