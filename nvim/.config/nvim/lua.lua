19eca9467d442d1cd23e5833f0f5fa046617c33f
Orestis Floros <orestis.floros@elastic.co>
2024-07-18 11:21:53 +0200

    nvim: lspconfig: gopls

--- nvim/.config/nvim/lua.lua
+++ nvim/.config/nvim/lua.lua
@@ -47,3 +47,7 @@ require('lspconfig')['pyright'].setup{
     on_attach = on_attach,
     flags = lsp_flags,
 }
+require('lspconfig')['gopls'].setup{
+    on_attach = on_attach,
+    flags = lsp_flags,
+}
