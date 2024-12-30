2aba8c83b99b722689e8b4d03528f19c44c45ef9
Orestis Floros <orestis.floros@elastic.co>
2024-12-30 15:42:32 +0100

    bin: format

--- executables/bin/i3-workspace-rename.py
+++ executables/bin/i3-workspace-rename.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import subprocess
+
 import i3ipc
 
 i3 = i3ipc.Connection()
@@ -10,10 +11,11 @@ num = focused.workspace().num
 
 if not num or num <= 0:
     import sys
+
     sys.exit(0)
 
-print(f'Renaming workspace number {num}')
+print(f"Renaming workspace number {num}")
 cmd = f"zenity --entry --entry-text='{focused.name}'"
 new_name = subprocess.check_output(cmd, shell=True).decode().strip()
-new_name = new_name.replace('"', r'\"')
+new_name = new_name.replace('"', r"\"")
 i3.command(f'rename workspace to "{num}:{new_name}"')
--- executables/bin/toggle-sticky.py
+++ executables/bin/toggle-sticky.py
@@ -6,13 +6,14 @@ import i3ipc
 def main(args):
     i3 = i3ipc.Connection()
     focused = i3.get_tree().find_focused()
-    if focused.sticky and focused.floating.endswith('on'):
-        focused.command('border normal, floating disable')
+    if focused.sticky and focused.floating.endswith("on"):
+        focused.command("border normal, floating disable")
     else:
-        focused.command('floating enable, sticky enable, border none')
+        focused.command("floating enable, sticky enable, border none")
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import sys
+
     sys.exit(main(sys.argv))
