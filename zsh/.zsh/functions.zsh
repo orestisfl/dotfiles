6c8245d8355104ecac9cd3f28102de1389e648db
Orestis Floros <orestisflo@gmail.com>
2025-11-06 13:07:29 +0100

    Switch to yazi

--- zsh/.zsh/functions.zsh
+++ zsh/.zsh/functions.zsh
@@ -168,3 +168,11 @@ jqfmt() {
   done
   return 0
 }
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
